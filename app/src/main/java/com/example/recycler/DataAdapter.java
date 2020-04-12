9eb293df92362915714941d7925516664774e905
MOHAMED-BAD <55801427+MOHAMED-BAD@users.noreply.github.com>
2020-04-12 23:38:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/recycler/DataAdapter.java
@@ -0,0 +1,83 @@
+package com.example.recycler;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class DataAdapter extends ListAdapter<Data,DataAdapter.ContactViewHolder> {
+    ImageView imageView;
+    static  DiffUtil.ItemCallback<Data> diffCallback=new DiffUtil.ItemCallback<Data>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Data oldItem, @NonNull Data newItem) {
+            return oldItem.getNumber().equals(newItem.getNumber());
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Data oldItem, @NonNull Data newItem) {
+            return oldItem.getName().equals(newItem.getName());
+        }
+    };
+
+    public DataAdapter(OnDataClicked onItemClickedListener) {
+        super(diffCallback);
+        this.onItemClickedListener = onItemClickedListener;
+    }
+
+    @NonNull
+    @Override
+    public ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_data, parent, false);
+        return new ContactViewHolder(view);
+    }
+
+    private OnDataClicked onItemClickedListener;
+    interface OnDataClicked {
+        void edit(Data data, int position);
+        void delet(Data data ,int position);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactViewHolder holder, final int position) {
+        holder.onBind(getItem(position));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClickedListener.edit(getItem(position), position);
+            }
+        });
+        imageView=holder.itemView.findViewById(R. id.img_delete);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onItemClickedListener.delet(getItem(position),position);
+
+            }
+        });
+    }
+
+    class ContactViewHolder extends RecyclerView.ViewHolder {
+        TextView textName, textNumber;
+
+        public ContactViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textName = itemView.findViewById(R.id.tv_name);
+            textNumber = itemView.findViewById(R.id.tv_number);
+        }
+
+        void onBind(Data contact) {
+            textName.setText(contact.getName());
+            textNumber.setText(contact.getNumber());
+        }
+    }
+}
